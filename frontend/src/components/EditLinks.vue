c77924fc9c400aabc5e6a79210c8a62f41ecfab3
treeoon <wisnuyeahfaizal@gmail.com>
2022-12-07 19:52:21 +0700

    new dashboard, cleaning

--- frontend/src/components/EditLinks.vue
+++ frontend/src/components/EditLinks.vue
@@ -75,7 +75,7 @@
         >
           &#8249;
         </button> -->
-        <button
+        <!-- <button
           type="button"
           @click="$router.push('/dashboard')"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -93,7 +93,7 @@
             />
           </svg>
           <span class="sr-only">Icon description</span>
-        </button>
+        </button> -->
       </div>
     </div>
   </div>
--- frontend/src/components/Shorten.vue
+++ frontend/src/components/Shorten.vue
@@ -1,31 +1,32 @@
 <template>
-
+<div class="flex w-screen h-screen justify-center items-center">
+    <p>Redirecting you to {{App.links.rawlinks}}.....</p>
+</div>
 </template>
 
 <script>
-import { collectionGroup } from '@firebase/firestore';
-import {useApp} from '../stores/index';
+import { useApp } from "../stores/index";
 
 export default {
   setup() {
     const App = useApp();
     return {
       App,
-    }
+    };
   },
   created() {
-    // let url = ""
-    // this.App.getlinks().then(() => {
-
-    //     this.App.links.forEach((link)=>{
-    //         // console.log(link)
-    //         if(this.$route.params.custom == link.customlinks) {
-    //             url = link.rawlinks
-    //         }
-    //     })
-    //     console.log(url)
-    //     window.location.href = url
-    // })
-  }
-}
+    let url = ""
+    this.App.getlinks().then(() => {
+        this.App.links.forEach((link)=>{
+            // console.log(link)
+            if(this.$route.params.custom == link.customlinks) {
+                url = link.rawlinks
+            }
+        })
+        console.log(url)
+        window.location.href = url
+    })
+    this.App.getlinks();
+  },
+};
 </script>
